357961a7e1d868af9bf1ed31e8def35dcea53c71
askatskov <160420092+askatskov@users.noreply.github.com>
2025-06-13 01:07:28 +0300

    TRP-3 Põhiliste teenuste ja konfiguratsioonide ettevalmistamine (Program.cs, appsettings.json)

--- TropicalFlow/Program.cs
+++ TropicalFlow/Program.cs
@@ -1,29 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Lisa teenused
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient(); // vajalik Spotify API jaoks
+builder.Services.AddSession();    // OAuth ja kasutaja sessioonide jaoks
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware pipeline
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
+app.UseSession(); // sessioonide toetus
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
+// Vaikimisi route
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
