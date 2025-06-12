fe094319e879492a4fcf03256099ae1a60e21cef
askatskov <160420092+askatskov@users.noreply.github.com>
2025-06-13 00:58:36 +0300

    Add project files.

---
+++ TropicalFlow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TropicalFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
