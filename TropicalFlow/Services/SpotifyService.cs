0e358e613b1a85eea25cacc05537cf6fd9e3ce14
askatskov <160420092+askatskov@users.noreply.github.com>
2025-06-13 01:26:15 +0300

    TRP-8 TASK-1.1: OAuth2 flow koodi kirjutamine

---
+++ TropicalFlow/Services/SpotifyService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Web;
+using System.Net.Http.Headers;
+
+public class SpotifyService
+{
+	private readonly IConfiguration _config;
+	private readonly IHttpClientFactory _httpClientFactory;
+
+	public SpotifyService(IConfiguration config, IHttpClientFactory httpClientFactory)
+	{
+		_config = config;
+		_httpClientFactory = httpClientFactory;
+	}
+
+	public string GetLoginUrl()
+	{
+		var clientId = _config["Spotify:ClientId"];
+		var redirectUri = _config["Spotify:RedirectUri"];
+		var scope = "user-read-currently-playing";
+
+		var query = HttpUtility.ParseQueryString(string.Empty);
+		query["client_id"] = clientId;
+		query["response_type"] = "code";
+		query["redirect_uri"] = redirectUri;
+		query["scope"] = scope;
+
+		return $"https://accounts.spotify.com/authorize?{query}";
+	}
+
+	public async Task<string> ExchangeCodeForTokenAsync(string code)
+	{
+		var clientId = _config["Spotify:ClientId"];
+		var clientSecret = _config["Spotify:ClientSecret"];
+		var redirectUri = _config["Spotify:RedirectUri"];
+
+		var http = _httpClientFactory.CreateClient();
+		var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
+
+		var keyValues = new List<KeyValuePair<string, string>>
+		{
+			new("grant_type", "authorization_code"),
+			new("code", code),
+			new("redirect_uri", redirectUri),
+			new("client_id", clientId),
+			new("client_secret", clientSecret)
+		};
+
+		request.Content = new FormUrlEncodedContent(keyValues);
+
+		var response = await http.SendAsync(request);
+		var responseContent = await response.Content.ReadAsStringAsync();
+
+		var json = System.Text.Json.JsonDocument.Parse(responseContent);
+		return json.RootElement.GetProperty("access_token").GetString();
+	}
+}
